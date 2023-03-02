7481857af2a791497e2208940b61659698f93fae
DesZafirova <deszafirova@icloud.com>
2023-03-02 16:03:41 +0200

    add some solution

--- src/restaurant/Food.java
+++ src/restaurant/Food.java
@@ -2,7 +2,14 @@ package restaurant;
 
 import java.math.BigDecimal;
 
-public class Food {
-    private String name;
-    private BigDecimal price;
+public class Food extends Product {
+    private double grams;
+    public Food(String name, BigDecimal price, double grams) {
+        super(name, price);
+        this.grams = grams;
+    }
+
+    public double getGrams() {
+        return grams;
+    }
 }
